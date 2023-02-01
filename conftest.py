c37afdb7d9c7f4b151b8c6c08394d022171d6ffb
DavidFreire-FEUP <up201806711@fe.up.pt>
2023-02-01 15:58:49 +0000

    allow not having tests without exiting pytest run

--- conftest.py
+++ conftest.py
@@ -1,2 +1,8 @@
 import sys
 sys.path.insert(0, "./src")
+
+
+# Avoid error return when no tests are collected
+def pytest_sessionfinish(session, exitstatus):
+    if exitstatus == 5:
+        session.exitstatus = 0
